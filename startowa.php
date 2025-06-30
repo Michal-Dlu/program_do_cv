3812eff6f46bafb5b7063eb4ac5505b5758eaeca
Michał Dłubak <turyferarz@tutanota.com>
2025-06-30 11:29:00 +0200

    poprawa zgodnośc ze standardem WCAG 2.0 (Level AAA) zamiana section na header i h2 na p

--- startowa.php
+++ startowa.php
@@ -16,29 +16,36 @@ $_SESSION['flaga']="strona4";}
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Startowa</title>
+    <title>Witamy na stronie Curriculum Vitae</title>
     <link rel="stylesheet" href="style.css">
 </head>
-<body dir="ltr">
-<section id="banner">
+<body>
+    
+<header>
     <h1>Witamy na stronie Curriculum Vitae</h1>
-</section>
+</header>
 <section id="question1">
-    <h2>Jesteś tu pierwszy raz?</h2>
+    <p class = "paragraf">Jesteś tu pierwszy raz?</p>
     <p>Proszę kliknąć na przycisk poniżej, aby przejść do formularza aplikacyjnego.</p>
 </section>
 <section id="qusetion2">
 <form method="post" action="Strona_1.php">
-    <input type="submit" value="Tak, przejdź dalej" aria-label="Przejdź dalej">
+    <input type="submit" value="Tak, przejdź dalej" aria-label="Przejdź dalej do wpypełniania aplikacji">
 </form>
 <br>
-<h2>Jestem tu po raz kolejny</h2>
+<p class = "paragraf">Jestem tu po raz kolejny</p>
 <p>Proszę wypełnić pola formularza poniżej wpisanymi wcześniej danymi, aby przejść do gotowego CV.</p>
 <form method="post" action="warunek.php" id="myForm">
-    <label for = "name">Imię: </label><input type = "text" placeholder="Wpisz imię" name="user_name" id="name" required aria-label="Imię"><br><br>
-    <label for = "surname">Nazwisko: </label><input type = "text" placeholder="Wpisz nazwisko" name="user_surname" id="surname" required aria-label="Nazwisko"><br><br>
-    <label for = "password">Hasło: </label><input type = "password" placeholder="Wpisz min.8-znakowe hasło" name="password" id="password" required aria-label="Hasło"><br><br>
-    <label for = "btn">Zatwierdź: </label><button type = "button" name="start" id="btn" aria-label="Zatwierdź">Przejdź do gotowego CV</button><br><br>
+    <label for = "name">Imię: </label>
+    <input type = "text" placeholder="Wpisz imię" name="user_name" id="name" required aria-label="Imię" aria-describedby="name-error">
+    <span id="name-error" class="error-message" aria-live="assertive"></span><br><br>
+    <label for = "surname">Nazwisko: </label><input type = "text" placeholder="Wpisz nazwisko" name="user_surname" id="surname" required aria-label="Nazwisko" aria-describedby="surname-error">
+    <span id="surname-error" class="error-message" aria-live="assertive"></span><br><br>
+    <br><br>
+    <label for = "password">Hasło: </label><input type = "password" placeholder="Wpisz min.8-znakowe hasło" name="password" id="password" required aria-label="Hasło" aria-describedby="password-error">
+    <span id="pass-error" class="error-message" aria-live="assertive"></span><br><br>
+    <br><br>
+    <label for = "btn">Zatwierdź: </label><button type = "button" name="start" id="btn" aria-label="Zatwierdź formularz">Przejdź do gotowego CV</button><br><br>
     <label for = "reset">Wyczyść dane: </label><input type = "reset" value="Wyczyść" id="reset" aria-label="Wyczyść dane formularza">
 </form>
 </section>
@@ -50,6 +57,37 @@ $_SESSION['flaga']="strona4";}
    
     let name = document.getElementById('name');
     let surname = document.getElementById('surname');
+
+    const inpName = document.getElementById('name');
+    const errName = document.getElementById('name-error');
+
+    inpName.addEventListener('blur', function(){
+        if(!inpName.value){
+             errName.textContent="Imię jest wymagane!";
+        }
+        else errName.textContent="";
+    })
+
+    const inpSurname = document.getElementById('surname');
+    const errSurname = document.getElementById('surname-error');
+
+    inpSurname.addEventListener('blur', function(){
+        if(!inpSurname.value){
+             errSurname.textContent="Nazwisko jest wymagane!";
+        }
+        else errSurname.textContent="";
+    })
+
+    
+    const inpPass = document.getElementById('password');
+    const errPass = document.getElementById('pass-error');
+
+    inpPass.addEventListener('blur', function(){
+        if(!inpPass.value){
+             errPass.textContent="Hasło jest wymagane!";
+        }
+        else errPass.textContent="";
+    })
 
     btn.addEventListener('click', function (){
      if(pass.value.length<8){
