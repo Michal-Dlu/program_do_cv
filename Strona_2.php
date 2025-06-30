cd6a67a3bd440f6cdfe0631cad28dd6ac4e41d9f
Michał Dłubak <turyferarz@tutanota.com>
2025-06-30 19:14:14 +0200

    skorygowano zdublowany kod; dodano header i main

--- Strona_2.php
+++ Strona_2.php
@@ -1,13 +1,14 @@
 <?php
 session_start();
-
 ?>
+
 <!DOCTYPE html>
 <html lang="pl">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Formularz wykszatłcenia</title>
+    <meta name="author" content="Michał Dłubak">
     <link rel="stylesheet" href="style.css">
 </head>
 <?php
@@ -16,12 +17,13 @@ if(!isset($_SESSION['corr']) && (isset($_SESSION['flaga']))){
 if($_SESSION['flaga']=="strona1"){
 
 print'
-
 <body>
+<header>
 <h1>Wykształcenie</h1>
-
+</header>
+<main>
     <form action="skrypt.php" method="POST">
-    <fieldset>  
+    <fieldset> <legend>Formularz wykształcenia</legend> 
     <input type="text" placeholder="Lata: " name="study"><br> 
     <input type="text" placeholder="Szkoła lub uczelnia" name="school"><br>
     <textarea placeholder="Opisz kierunek, tytuł naukowy" name="academic"></textarea>
@@ -30,7 +32,7 @@ print'
     <input type="submit" value="Przejdź dalej" name="ok1"><input type="reset" value="Wyczyść"> 
     </fieldset> 
     </form>
-
+</main>
 </body>
 </html>';}
 else{header('location:Strona_1.php');}
@@ -55,7 +57,10 @@ else if(isset($_SESSION['corr']) && isset($_SESSION['flaga'])){
     $stmt=$pdo->prepare($sql);
     $stmt->execute($params);
     $i=1;
-    print '<h1>Wykształcenie</h1>
+    print 
+    '
+    <h1>Wykształcenie</h1>  
+
     Zmień liniję: <br><br>
 <form action="" method="POST"> 
         <fieldset>
@@ -120,136 +125,8 @@ print"
 </form>";
 
 
-print "</body>
-</html>";
-}    
-}
-else{header('location:Strona_1.php');}
-}
-else{header('location:Strona_1.php');}
-
-?>
-
-
-<!DOCTYPE html>
-<html lang="pl">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-    <link rel="stylesheet" href="style.css">
-</head>
-<?php
-
-if(!isset($_SESSION['corr']) && (isset($_SESSION['flaga']))){
-if($_SESSION['flaga']=="strona1"){
-
-print'
-
-<body>
-<h1>Wykształcenie</h1>
-
-    <form action="skrypt.php" method="POST">
-    <fieldset>  
-    <input type="text" placeholder="Lata: " name="study"><br> 
-    <input type="text" placeholder="Szkoła lub uczelnia" name="school"><br>
-    <textarea placeholder="Opisz kierunek, tytuł naukowy" name="academic"></textarea>
-    <input type="submit" value="Dodaj kolejną szkołę" name="Add_data"><br><br>
-    Gdy wszystkie szkoły już zostały dodane kliknij wyślij<br><br>
-    <input type="submit" value="Przejdź dalej" name="ok1"><input type="reset" value="Wyczyść"> 
-    </fieldset> 
-    </form>
-
+print "</main>
 </body>
-</html>';}
-else{header('location:Strona_1.php');}
-}
-else if(isset($_SESSION['corr']) && isset($_SESSION['flaga'])){
-  if($_SESSION['flaga']=="strona1") {
-   
-    try{
-    $host='localhost';
-    $dbname = 'curiculum';
-    $user = 'root';
-    $password = "";
-    $pdo = new PDO("mysql:host=$host;dbname=$dbname",$user,$password);
-    $pdo-> SetAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
-    }
-    catch(PDOException $e){
-        die('Błąd połącznia z bazą danych: '.$e->Message());
-    }
-
-    $sql="Select * from wyksztalcenie where cv = ? order by id desc";
-    $params = [$_SESSION['id']];
-    $stmt=$pdo->prepare($sql);
-    $stmt->execute($params);
-    $i=1;
-    print '<h1>Wykształcenie</h1>
-    Zmień liniję: <br><br>
-<form action="" method="POST"> 
-        <fieldset>
-        <legend>Wybierz wcześniejsze wpisy do poprawy</legend>
-        <select name="del" style="font-size:10px;" id="corr_select">';
-    while($result = $stmt->fetch(PDO::FETCH_ASSOC)){
-        $id= $result['id'];
-        $study = $result['study'];
-        $school = $result['school'];
-        $academic = $result['academic'];
-        $cv= $result['cv'];    
-       
-        print '<option value="'.$id.'">'.$i."."." ".$study." ".$school." ".$academic.'<br>'.'</option>';        
-       $i++;
-    }
-        print'</select>';
-                         
-        print '<p><input type="submit" name= "inp_corr" value="Do poprawy"></p>
-  </fieldset>
-</form><br><br><br>';
-
-print '<form action="skrypt.php" method="POST">
-    <fieldset>
-    <legend>Dodaj kolejną pozycję</legend>  
-    <input type="text" placeholder="Lata: " name="study"><br> 
-    <input type="text" placeholder="Szkoła lub uczelnia" name="school"><br>
-    <textarea placeholder="Opisz kierunek, tytuł naukowy" name="academic"></textarea>
-    <input type="submit" value="Dodaj kolejną szkołę" name="Add_next_data"><br><br>
-    Gdy wszystkie szkoły już zostały dodane kliknij wyślij<br><br>
-    <input type="submit" value="Przejdź dalej" name="next_ok1"><input type="reset" value="Wyczyść"> 
-    </fieldset> 
-    </form>';
-
-
-if(isset($_POST['inp_corr'])){
-    $corr_id = $_POST['del'];
-    $_SESSION['corr_id']=$corr_id;
-    $sql="Select * from wyksztalcenie where id = ? and cv = ?";
-    $params = [$corr_id, $_SESSION['id']];
-    $stmt=$pdo->prepare($sql);
-    $stmt->execute($params);
-
-while ($result = $stmt->fetch(PDO::FETCH_ASSOC)){
-$study=$result['study'];
-$school=$result['school'];
-$academic=$result['academic'];
-}
-print"
-<body>
-<h1>Wykształcenie</h1>
-
-    <form action='skrypt.php' method='POST'><br>
-    <fieldset> <legend>Popraw pozycję</legend>  
-    
-    <input type='text' placeholder='Lata: ' name='study'  value='$study'><br> 
-    <input type='text' placeholder='Szkoła lub uczelnia' name='school'  value='$school'><br>
-
-    <textarea placeholder='Opisz kierunek, tytuł naukowy' name='academic' >$academic</textarea><br>
-    
-    <input type='submit' value='Popraw' name='input_corr'><br><br><br>
-    </fieldset>
-</form>";
-
-
-print "</body>
 </html>";
 }    
 }
@@ -258,4 +135,6 @@ else{header('location:Strona_1.php');}
 else{header('location:Strona_1.php');}
 
 ?>
+
+
 
--- startowa.php
+++ startowa.php
@@ -16,6 +16,7 @@ $_SESSION['flaga']="strona4";}
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="author" content="Michał Dłubak">
     <title>Witamy na stronie Curriculum Vitae</title>
     <link rel="stylesheet" href="style.css">
 </head>
